e0df03fcd76c9c40182e5ad7e2cc20f4a1568b8b
code4simulation <161325392+code4simulation@users.noreply.github.com>
2024-03-04 20:42:29 +0900

    Update showmap.js

--- src/showmap.js
+++ src/showmap.js
@@ -1,23 +1,9 @@
-var hall_name = "<b> 아펠가모 잠실 </b>";
-var hall_lat  = 37.515983;
-var hall_long = 127.0995839;
-var c_lat  = 37.51475;
-var c_long = 127.10030;
-var zoom = 17;
-
-var mymap = L.map('map').setView([c_lat, c_long], zoom);
+var mymap = L.map('map').setView([info.center_lat, info.center_long], info.map_zoom);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: zoom,
+  maxZoom: info.map_zoom,
 }).addTo(mymap);
 
 // Add a marker for the wedding hall
-var marker = L.marker([hall_lat, hall_long]).addTo(mymap);
-marker.bindPopup(hall_name).openPopup();
-
-// This ensures the map is displayed correctly after being initially hidden
-mymap.on('load', function() {
-  setTimeout(function() {
-    mymap.invalidateSize();
-  }, 400);
-});
+var marker = L.marker([info.hall_lat, info.hall_long]).addTo(mymap);
+marker.bindPopup(info.WeddingHall).openPopup();
