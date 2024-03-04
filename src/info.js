c8df3a81a6aad9d592e7412b05cb4ca6b9b0d491
code4simulation <161325392+code4simulation@users.noreply.github.com>
2024-03-04 20:42:14 +0900

    Create info.js

---
+++ src/info.js
@@ -0,0 +1,16 @@
+const info = {
+  // 결혼 날짜, 신랑, 신부, 연락처, 계좌 등 정보를 입력하세요.
+  "WeddingDate": "2024-05-20",
+  "Groom": "Sin-Rang",
+  "GroomPhone": "01012345678",
+  "Bride": "Sin-Bu",
+  "BridePhone": "01087654321",
+
+  // 장소에 관련된 정보들을 입력하세요
+  "WeddingHall": "<b> 아펠가모 잠실 </b>",
+  "hall_lat":  37.515983,
+  "hall_long": 127.0995839,
+  "center_lat":  37.515983,
+  "center_long": 127.0995839,
+  "map_zoom":  17
+}
