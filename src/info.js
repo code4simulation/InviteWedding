6e18f7156535d614f6ec1cebc82aa6ebcd318a96
code4simulation <161325392+code4simulation@users.noreply.github.com>
2024-04-23 22:13:31 +0900

    Add files via upload

---
+++ src/info.js
@@ -0,0 +1,24 @@
+const info = {
+  "Greetings": "봄바람을 타고 설렘과 함께 찾아온 4월</br>두 가족의 첫 만남을 시작하려 합니다.</br></br>두 사람의 만남을 지켜봐주시고</br>응원해주셔서 감사합니다.</br></br>함께하는 첫 걸음에 양가 가족분들을 모시고</br>작은 식사 자리를 마련하고자 합니다.</br>소중한 만남의 자리를 빛내주세요.</br></br>",
+  // 결혼 날짜, 신랑, 신부, 연락처, 계좌 등 정보를 입력하세요.
+  "WeddingDate": "2024-04-27T17:30:00",
+  "WeddingHall": "<b>호생원</b>",
+  "PlaceAddress": "경기 용인시 기흥구 기흥단지로 58",
+  "Groom": "이동헌",
+  "GroomDad": "이성찬",
+  "GroomMom": "이상순",
+  "Bride": "이승아",
+  "BrideDad": "이왕식",
+  "BrideMom": "김향선",
+
+  
+  // 장소에 관련된 정보들을 입력하세요
+  "Naver": "https://naver.me/xV3xjR82",
+  "Google": "https://maps.app.goo.gl/UkBXWxTfkrPcWi6k8",
+  "Kakao": "https://kko.to/f9-dvKDJso",
+  "hall_lat":  37.515983,
+  "hall_long": 127.0995839,
+  "center_lat":  37.515983,
+  "center_long": 127.0995839,
+  "map_zoom":  17
+}
